4630709ede846841800523c21edebee8bda6fa38
Tristan Cacqueray <tdecacqu@redhat.com>
2021-06-06 14:00:08 +0000

    Add blog post about haskell use cases
    Change-Id: I748e1f1a361c76d003aaf7294a8f370d57c3cd0f

---
+++ src/blog-practical-haskell-use-cases.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -e
+
+NAME="blog-practical-haskell-use-cases"
+
+pandoc --include-in-header=./$NAME.rst \
+       -f gfm --reference-links  \
+       -t rst ./$NAME.md -o ../website/content/$NAME.rst
